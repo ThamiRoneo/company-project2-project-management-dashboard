21afc4de29a620f86c4cf5d3e2e702354a522492
lesedi <analystmodikwe18@gmail.com>
2026-07-30 00:15:26 +0200

    project progress

---
+++ taskflow/src/utils/projectProgress.ts
@@ -0,0 +1,16 @@
+import type { Project } from "../types/index.ts";
+
+
+// The helper to calculate progress on the dashboard
+
+// declaring a function  project matching Project interface from types that will return a number which is the progress percentage and if the project has zero tasks return 0%
+
+export function getProjectProgress(project: Project): number {
+    if (project.tasks.length ===0) return 0;
+
+    // .filter() loops through every task and keep the ones where status is === completed .length will count how many tasks are done
+    const completed = project.tasks.filter((t) => t.status === "completed").length;
+
+    // percentage calculation. Math.round will round off everything to a whole number
+    return Math.round((completed / project.tasks.length) * 100);
+}
