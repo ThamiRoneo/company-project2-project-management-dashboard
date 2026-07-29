9720593e60383a8236cbe69598a5bd929749ad7f
lesedi <analystmodikwe18@gmail.com>
2026-07-29 11:47:22 +0200

    adding and cleaning up

--- taskflow/src/components/projects/ProjectCard.tsx
+++ taskflow/src/components/projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import { getStatusColor, getStatusLabel } from "../../utils/getStatusColor.ts";
 import type { Project } from "../../types/index.ts";
 import { getProjectProgress } from "../../utils/projectProgress.ts";
 
@@ -5,23 +6,6 @@ interface ProjectCardProps {
   project: Project;
   onClick?: (id: string) => void;
 }
-
-// showing different color based on the status of the project
-
-//  Record<Project["status"], string>  is a TypeScript syntax meaning "an object whose keys must be exactly the values of Project["status"], and whose values are strings." It forces you to handle every possible status
-
-const statusColors: Record<Project["status"], string> = {
-  active: "bg-green-100 text-green-700",
-  completed: "bg-blue-100 text-blue-700",
-  on_hold: "bg-yellow-100 text-yellow-700",
-};
-
-// changing Projectstatus on index.ts labels from small to capital letters
-const statusLabels: Record<Project["status"], string> = {
-  active: "Active",
-  completed: "Completed",
-  on_hold: "On Hold",
-};
 
 
 // the component
@@ -40,10 +24,10 @@ export default function ProjectCard({ project, onClick }: ProjectCardProps) {
             <h3 className="font-semibold text-lg text-gray-800">{project.name}</h3>
 
             {/* this template literal will look up the right color classes based on the specific project current status  */}
-            <span className={`text-xs px-2 py-1 rounded-full font-medium ${statusColors[project.status]}`}>
+            <span className={`text-xs px-2 py-1 rounded-full font-medium ${getStatusColor(project.status)}`}>
 
-                {/* prints the readable label */}
-                {statusLabels[project.status]}
+                {/* prints the readable label/status */}
+                {getStatusLabel(project.status)}
             </span>
         </div>
 
