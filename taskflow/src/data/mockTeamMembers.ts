1a419a5a15315c8a96a5a4265b7319d7cd2372a5
galaletsangmodise <galaletsangmodise71@gmail.com>
2026-07-29 09:26:48 -0700

    feat: add mock team members data for development

---
+++ taskflow/src/data/mockTeamMembers.ts
@@ -0,0 +1,34 @@
+import { type TeamMember } from "../types";
+
+export const mockTeamMembers: TeamMember[] = [
+  {
+    id: "user-1",
+    name: "Lerato",
+    avatar: "https://i.pravatar.cc/150?img=1",
+    role: "Backend Developer",
+  },
+  {
+    id: "user-2",
+    name: "Thami",
+    avatar: "https://i.pravatar.cc/150?img=2",
+    role: "Full Stack Developer",
+  },
+  {
+    id: "user-3",
+    name: "Gareth",
+    avatar: "https://i.pravatar.cc/150?img=3",
+    role: "Frontend Developer",
+  },
+  {
+    id: "user-4",
+    name: "Lesedi",
+    avatar: "https://i.pravatar.cc/150?img=4",
+    role: "Project Manager",
+  },
+{
+    id: "user-5",
+    name: "Galaletsang",
+    avatar: "https://i.pravatar.cc/150?img=1",
+    role: "Frontend Developer",
+  }
+];
