685b9dfb4d49273bcd5bf58458d7357ee48f581c
lesedi <analystmodikwe18@gmail.com>
2026-07-31 14:19:02 +0200

    format date

---
+++ taskflow/src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+export function formatDate(dateString: string): string {
+  return new Date(dateString).toLocaleDateString(undefined, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+// lesedi
