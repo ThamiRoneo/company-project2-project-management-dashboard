5d6477c3315e6fcef89f4ee75aa777c3261da6a0
ThamiRoneo <thamironeo,sithole@icloud.com>
2026-08-01 06:29:29 +0200

    refactor: reverted to an empty state

--- taskflow/src/components/ui/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center p-8">
-      <div className="w-8 h-8 border-4 border-indigo-200 border-t-indigo-600 rounded-full animate-spin" />
-    </div>
-  );
-}
